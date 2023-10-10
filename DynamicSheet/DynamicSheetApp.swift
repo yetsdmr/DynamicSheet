37a8975db77c4c9047058688c2e9057a96ab0d97
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-10 10:36:27 +0300

    Initial Commit

---
+++ DynamicSheet/DynamicSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicSheetApp.swift
+//  DynamicSheet
+//
+//  Created by Yunus Emre Taşdemir on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DynamicSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
